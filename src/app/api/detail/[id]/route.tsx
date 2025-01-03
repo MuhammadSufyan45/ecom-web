e4403553e0d0cb3e6de4f974d72b9f9d8fe0adf0
MuhammadSufyan45 <syedsufyan4585@gmail.com>
2025-01-03 23:13:16 +0500

    'lastly'

--- src/app/api/detail/[id]/route.tsx
+++ src/app/api/detail/[id]/route.tsx
@@ -1,13 +1,9 @@
 import { Data } from "@/data/data";
 import { NextRequest, NextResponse } from "next/server";
 
-interface Content {
-    params: {
-        id: string;
-    };
-}
 
-export async function GET(request: NextRequest, content : Content) {
-  const detail = Data.filter(item => item.id === content.params.id);
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+  const { id } = params; 
+  const detail = Data.filter(item => item.id === id);
   return NextResponse.json(detail.length === 0 ? { result: "No data found" } : { result: detail });
 }
