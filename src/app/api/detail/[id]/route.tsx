6ce944a5adb3f5d7c7c44f371f1c3d05344410a4
MuhammadSufyan45 <syedsufyan4585@gmail.com>
2025-01-03 22:49:41 +0500

    'error'

--- src/app/api/detail/[id]/route.tsx
+++ src/app/api/detail/[id]/route.tsx
@@ -1,7 +1,8 @@
 import { Data } from "@/data/data";
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(request: NextRequest, { params }: { params: { id: string } }) {
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
     const detail = Data.filter(item => item.id == params.id);
-    return NextResponse.json(detail.length == 0 ? { result: "No data found" } : { result: detail });
+    return NextResponse.json(detail.length === 0 ? { result: "No data found" } : { result: detail });
 }
