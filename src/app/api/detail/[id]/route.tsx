8135f9af491c7a0e5296efbbfac8899e4770d0bf
MuhammadSufyan45 <syedsufyan4585@gmail.com>
2025-01-03 23:20:12 +0500

    'solved'

--- src/app/api/detail/[id]/route.tsx
+++
@@ -1,9 +0,0 @@
-import { Data } from "@/data/data";
-import { NextRequest, NextResponse } from "next/server";
-
-
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params; 
-  const detail = Data.filter(item => item.id === id);
-  return NextResponse.json(detail.length === 0 ? { result: "No data found" } : { result: detail });
-}
