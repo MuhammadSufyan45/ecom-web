3f543dcf6397375ca69c3f5899add725f112585d
MuhammadSufyan45 <syedsufyan4585@gmail.com>
2025-01-03 23:02:32 +0500

    'last'

--- src/app/api/detail/[id]/route.tsx
+++ src/app/api/detail/[id]/route.tsx
@@ -1,8 +1,13 @@
 import { Data } from "@/data/data";
 import { NextRequest, NextResponse } from "next/server";
 
+interface Content {
+    params: {
+        id: string;
+    };
+}
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-  const detail = Data.filter(item => item.id === params.id);
+export async function GET(request: NextRequest, content : Content) {
+  const detail = Data.filter(item => item.id === content.params.id);
   return NextResponse.json(detail.length === 0 ? { result: "No data found" } : { result: detail });
 }
